5a162b65def8f77f57bce23e7248bbbe3ebc1470
Karla Carballo <karlacarballo@gmail.com>
2019-02-20 18:19:22 -0800

    consolidated from multiple options into one form, should only have two fields to put text into, option to pick 'add, subtract, multiply or divide' and 'go' button.

--- js/scripts.js
+++ js/scripts.js
@@ -17,43 +17,26 @@ var divide = function(number1, number2) {
 
 // Everything below this line is user interface (or front-end) logic:
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    debugger;
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
-    $("#output-add").text(result);
-
-  event.preventDefault();
-});
-
-    $("form#subtract").submit(function(event) {
-      var number1 = parseInt($("#subtract1").val());
-      var number2 = parseInt($("#subtract2").val());
-      var result = subtract(number1, number2);
-      $("#output-subtract").text(result);
-
+  $("form#calculator").submit(function() {
     event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    // console.log("1st number: " + number1); // for dubugging
+    // console.log("2nd number: " + number2); // for dubugging
+    // console.log("operator: " + operator); // for dubugging
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+
+  // debugger;
   });
-
-  $("form#multiply").submit(function(event) {
-    debugger;
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = multiply(number1, number2);
-    $("#output-multiply").text(result);
-
-  event.preventDefault();
-});
-
-$("form#divide").submit(function(event) {
-  debugger;
-  var number1 = parseInt($("#divide1").val());
-  var number2 = parseInt($("#divide2").val());
-  var result = divide(number1, number2);
-  $("#output-divide").text(result);
-
-event.preventDefault();
-});
-
 });
